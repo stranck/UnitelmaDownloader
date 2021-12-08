9659675d17f6fcb5c63356a9feef91c4dab9e42a
Luca Sorace "Stranck <strdjn@gmail.com>
2021-12-08 15:53:30 +0100

    Code cleanup

--- unitelmaDownloader.py
+++ unitelmaDownloader.py
@@ -24,9 +24,6 @@ def verbose():
 	requests_log.setLevel(logging.DEBUG)
 	requests_log.propagate = True
 
-	#requests.get('https://httpbin.org/headers')
-#verbose()
-
 HELP = '''
 >>>> HELP MENU <<<<
 
@@ -73,12 +70,6 @@ UnitelmaDownloader creato da https://stranck.ovh
 '''
 
 VERBOSE = False
-#USERNAME = ""
-#PASSWORD = ""
-
-#COURSE_ID = 0 #1888
-#VIDEO_LINK = "" #"https://elearning.unitelma.it/mod/kalvidres/view.php?id=50969"
-
 session = requests.Session()
 
 def convert_size(size_bytes):
@@ -130,7 +121,6 @@ def selectStream(streams, params):
 
 def analyzeParam(args):
 	link = args.link[0] if args.link is not None else None
-	#courseid = -1
 	modeAnd = not args.modeOr
 	getInfo = args.getInfo
 	filters = {}
@@ -142,38 +132,11 @@ def analyzeParam(args):
 			key = f[0]
 			value = f[1]
 			filters[key] = value
-	'''
-	i = 1
-	while(i < len(args)):
-		arg = args[i].lower()
-		if(arg == "--link"):
-			i += 1
-			link = args[i]
-		#elif(arg == "--courseid"):
-		#	i += 1
-		#	courseid = int(args[i].strip())
-		elif(arg == "--modeAnd"):
-			modeAnd = True
-		elif(arg == "--modeOr"):
-			modeAnd = False
-		elif(arg == "--filter"):
-			i += 1
-			key = args[i]
-			i += 1
-			value = args[i]
-			filters[key] = value
-		elif(arg == "--getInfo"):
-			getInfo = True
-		elif(arg == "--fileName"):
-			i += 1
-			fileName = args[i]
-		
-		i += 1'''
+
 	if(len(filters) == 0):
 		getInfo = True
 	obj = {
 		"link": link,
-		#"courseId": courseid,
 		"modeAnd": modeAnd,
 		"filters": filters,
 		"getInfo": getInfo,
@@ -231,47 +194,6 @@ def analyzeArgs():
 	if(verboseLevel > 1):
 		verbose()
 
-	'''i = 1
-	#TODO permit classic arg passing like -vv, or -uUSERNAME
-	while(i < len(args)):
-		arg = args[i].lower()
-		if(arg == "--help"):
-			print(HELP)
-			exit(1)
-		elif(arg == "--username" or arg == "-u"):
-			i += 1
-			usr = args[i]
-		elif(arg == "--password" or arg == "-p"):
-			i += 1
-			pw = args[i]
-		elif(arg == "--file" or arg == "-f"):
-			i += 1
-			fileName = args[i]
-			with open(fileName) as file:
-				n = 0
-				for line in file:
-					line = line.strip()
-					sp = line.split()
-					param = analyzeParam(sp)
-					if(param == None):
-						print(f"*** AN ERROR OCCURRED LOADING PARAM AT LINE {n}")
-					else:
-						params.append(param)
-					n += 1
-		elif(arg == "--verbose" or arg == "-v"):
-			if(verboseLevel == 0):
-				VERBOSE = True
-				verboseLevel += 1
-			elif(verboseLevel == 1):
-				verbose()
-				verboseLevel += 1
-		#else:
-		#	print(f"Unrecognized argoument: {arg}\n\n")
-		#	print(HELP)
-		#	exit(1)
-		
-		i += 1'''
-
 	param = analyzeParam(args)
 	if(param != None):
 		params.append(param)
@@ -284,43 +206,13 @@ def analyzeArgs():
 
 def login(session, usr, pw):
 	try: 
-		#print(usr, pw)
 		if VERBOSE:
 			print("Logging in...", end="")
-		'''
-		curl 'https://idp.unitelma.it/idp/profile/SAML2/Redirect/SSO?execution=e1s2' \
-	-H 'Connection: keep-alive' \
-	-H 'Pragma: no-cache' \
-	-H 'Cache-Control: no-cache' \
-	-H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"' \
-	-H 'sec-ch-ua-mobile: ?0' \
-	-H 'sec-ch-ua-platform: "Windows"' \
-	-H 'Upgrade-Insecure-Requests: 1' \
-	-H 'Origin: https://idp.unitelma.it' \
-	-H 'Content-Type: application/x-www-form-urlencoded' \
-	-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36' \
-	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
-	-H 'Sec-Fetch-Site: same-origin' \
-	-H 'Sec-Fetch-Mode: navigate' \
-	-H 'Sec-Fetch-User: ?1' \
-	-H 'Sec-Fetch-Dest: document' \
-	-H 'Referer: https://idp.unitelma.it/idp/profile/SAML2/Redirect/SSO?execution=e1s2' \
-	-H 'Accept-Language: en-US,en;q=0.9' \
-	-H 'Cookie: JSESSIONID=B860612B6E5BEF8046F6BADBB0A23CC1.idp-unitelma.prod-idpbe141' \
-	--data-raw 'j_username=corso.informatica&j_password=informatica&_eventId_proceed=' \
-	--compressed
-		'''
-		
-		#params = {
-		#	"j_username": USERNAME,
-		#	"j_password": PASSWORD,
-		#	"_eventId_proceed": ""
-		#}
+
 		heahder = {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
 			#"Origin": "https://idp.unitelma.it"
 		}
-		#print("\n\n#1 =========================================================\n")
 		r = session.get("https://elearning.unitelma.it/", headers=heahder)
 		if(VERBOSE):
 			print(".", end="")
@@ -328,7 +220,6 @@ def login(session, usr, pw):
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
 			"Referer": "https://elearning.unitelma.it/unitelma_login.php"
 		}
-		#print("\n\n#2 =========================================================\n")
 		r = session.get("https://elearning.unitelma.it/auth/shibboleth/index.php?", headers=heahder)
 		if(VERBOSE):
 			print(".", end="")
@@ -347,7 +238,6 @@ def login(session, usr, pw):
 			"shib_idp_ls_supported": "true",
 			"_eventId_proceed": ""
 		}
-		#print("\n\n#3 =========================================================\n")
 		r = session.post("https://idp.unitelma.it/idp/profile/SAML2/Redirect/SSO?execution=e1s1", headers=heahder, data=params)
 		if(VERBOSE):
 			print(".", end="")
@@ -355,8 +245,6 @@ def login(session, usr, pw):
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
 			"Referer": "https://idp.unitelma.it/idp/profile/SAML2/Redirect/SSO?execution=e1s1"
 		}
-		#print("\n\n#4 =========================================================\n")
-		#r = session.post("https://idp.unitelma.it/idp/profile/SAML2/Redirect/SSO?execution=e1s2", headers=heahder, data=params)
 		heahder = {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
 			"Origin": "https://idp.unitelma.it",
@@ -367,11 +255,9 @@ def login(session, usr, pw):
 			"j_password": pw,
 			"_eventId_proceed": ""
 		}
-		#print("\n\n#5 =========================================================\n")
 		r = session.post("https://idp.unitelma.it/idp/profile/SAML2/Redirect/SSO?execution=e1s2", data=params, headers=heahder)
 		if(VERBOSE):
 			print(".", end="")
-		#r = session.get("https://elearning.unitelma.it/")
 		t = r.text
 		relayState = "cookie:" + t.split('"RelayState" value="cookie&#x3a;')[1].split('"/>')[0]
 		resp = t.split('"SAMLResponse" value="')[1].split('"/>')[0]
@@ -385,7 +271,6 @@ def login(session, usr, pw):
 			"SAMLResponse": resp
 		}
 		r = session.post("https://elearning.unitelma.it/Shibboleth.sso/SAML2/POST", data=params, headers=heahder, allow_redirects=True)
-		#print(r.text)
 		if("Non sei collegato" in r.text):
 			if(VERBOSE):
 				print(" ", end="")
@@ -513,31 +398,6 @@ def getKs(session, videoLink, mainId, courseId):
 def getStreams(session, mainId, ksToken):
 	if(VERBOSE):
 		print("Asking for streams metadata...", end=" ")
-	'''curl -i -X POST \
-   -H "Content-Type:application/x-www-form-urlencoded" \
-   -d "apiVersion=3.1" \
-   -d "expiry=86400" \
-   -d "clientTag=kwidget%3Av2.81.4" \
-   -d "1:ks=djJ8MTEzfD_Ht59bsunOL8vuaAP8tORMEabxKc258eyF_YvqCwKHM69hFCjOrXwfmyrVp2ShjAFFJVINd_Bz0CQmB8SaU70KTchB0oaEwztB-YEyR15675mxu9QubYthzU95-9wBJtv8g07TJDuB2rPY0f2VHwlmCj__MNTzOVcZl5RXR16PMr4mNj3NVS8r5PhMj8AcMlIrzhGJe4WU03vhXeyo_xPjxC_8LRjHQGZvxOcAIV3zotX1IYEyhndnteivbR3oKsxS9vW-gAJSxOXAjVLJzwooOAb67ytNkaTM3bi9U2UzEvoH5FoIYpBa4_aO_aGnNNz6dEDrcyphEEDq6wTxBFVVxqP3UCkl0fDYeGBj48Ts4N7cR52JWzNmDu0f3epK6TuV1ATdQG0NKXil6F8Uu9D5Cf0nRoZwh5B41q6AUhAcICbhyQnQxhMTApj52UCCw7BcHnQ34B89JyjqyZYta3LcrF1yEw2fljd2Q7Xgip13" \
-   -d "1:service=baseEntry" \
-   -d "1:action=list" \
-   -d "1:filter:objectType=KalturaBaseEntryFilter" \
-   -d "1:filter:typeEqual=1" \
-   -d "1:filter:parentEntryIdEqual=0_hbyn9aso" \
-   -d "format=1" \
-   -d "2:ks=djJ8MTEzfD_Ht59bsunOL8vuaAP8tORMEabxKc258eyF_YvqCwKHM69hFCjOrXwfmyrVp2ShjAFFJVINd_Bz0CQmB8SaU70KTchB0oaEwztB-YEyR15675mxu9QubYthzU95-9wBJtv8g07TJDuB2rPY0f2VHwlmCj__MNTzOVcZl5RXR16PMr4mNj3NVS8r5PhMj8AcMlIrzhGJe4WU03vhXeyo_xPjxC_8LRjHQGZvxOcAIV3zotX1IYEyhndnteivbR3oKsxS9vW-gAJSxOXAjVLJzwooOAb67ytNkaTM3bi9U2UzEvoH5FoIYpBa4_aO_aGnNNz6dEDrcyphEEDq6wTxBFVVxqP3UCkl0fDYeGBj48Ts4N7cR52JWzNmDu0f3epK6TuV1ATdQG0NKXil6F8Uu9D5Cf0nRoZwh5B41q6AUhAcICbhyQnQxhMTApj52UCCw7BcHnQ34B89JyjqyZYta3LcrF1yEw2fljd2Q7Xgip13" \
-   -d "2:service=flavorAsset" \
-   -d "2:action=list" \
-   -d "2:filter:entryIdEqual={1:result:objects:0:id}" \
-   -d "3:ks=djJ8MTEzfD_Ht59bsunOL8vuaAP8tORMEabxKc258eyF_YvqCwKHM69hFCjOrXwfmyrVp2ShjAFFJVINd_Bz0CQmB8SaU70KTchB0oaEwztB-YEyR15675mxu9QubYthzU95-9wBJtv8g07TJDuB2rPY0f2VHwlmCj__MNTzOVcZl5RXR16PMr4mNj3NVS8r5PhMj8AcMlIrzhGJe4WU03vhXeyo_xPjxC_8LRjHQGZvxOcAIV3zotX1IYEyhndnteivbR3oKsxS9vW-gAJSxOXAjVLJzwooOAb67ytNkaTM3bi9U2UzEvoH5FoIYpBa4_aO_aGnNNz6dEDrcyphEEDq6wTxBFVVxqP3UCkl0fDYeGBj48Ts4N7cR52JWzNmDu0f3epK6TuV1ATdQG0NKXil6F8Uu9D5Cf0nRoZwh5B41q6AUhAcICbhyQnQxhMTApj52UCCw7BcHnQ34B89JyjqyZYta3LcrF1yEw2fljd2Q7Xgip13" \
-   -d "3:service=flavorAsset" \
-   -d "3:action=list" \
-   -d "3:filter:entryIdEqual={1:result:objects:1:id}" \
-   -d "4:ks=djJ8MTEzfD_Ht59bsunOL8vuaAP8tORMEabxKc258eyF_YvqCwKHM69hFCjOrXwfmyrVp2ShjAFFJVINd_Bz0CQmB8SaU70KTchB0oaEwztB-YEyR15675mxu9QubYthzU95-9wBJtv8g07TJDuB2rPY0f2VHwlmCj__MNTzOVcZl5RXR16PMr4mNj3NVS8r5PhMj8AcMlIrzhGJe4WU03vhXeyo_xPjxC_8LRjHQGZvxOcAIV3zotX1IYEyhndnteivbR3oKsxS9vW-gAJSxOXAjVLJzwooOAb67ytNkaTM3bi9U2UzEvoH5FoIYpBa4_aO_aGnNNz6dEDrcyphEEDq6wTxBFVVxqP3UCkl0fDYeGBj48Ts4N7cR52JWzNmDu0f3epK6TuV1ATdQG0NKXil6F8Uu9D5Cf0nRoZwh5B41q6AUhAcICbhyQnQxhMTApj52UCCw7BcHnQ34B89JyjqyZYta3LcrF1yEw2fljd2Q7Xgip13" \
-   -d "4:service=flavorAsset" \
-   -d "4:action=list" \
-   -d "4:filter:entryIdEqual={1:result:objects:0:parentEntryId}" \
- "https://kmc.l2l.cineca.it/api_v3/index.php?service=multirequest"'''
 	headers = {
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
 		"Referer": "https://kaf-113prod.elearning.unitelma.it/",
@@ -577,7 +437,6 @@ def getStreams(session, mainId, ksToken):
 	streams = []
 	i = 0
 	for stream in j:
-		#print(stream)
 		highRes = stream["objects"][0]
 		tags = set()
 
@@ -636,7 +495,6 @@ def getDownloadLink(stream):
 def downloadVideo(param):
 	videoLink = param["link"]
 	print("\nDownloading: " + videoLink)
-	#courseId = param["courseId"]
 	mainId, courseId = getMainID(session, videoLink)
 	ksToken = getKs(session, videoLink, mainId, courseId)
 	streams = getStreams(session, mainId, ksToken)
@@ -689,10 +547,3 @@ if __name__ == "__main__":
 		if(VERBOSE):
 			print("Executing " + cmd)
 		os.system(cmd)
-
-#kafEndpoint, kafRefer = getKaf(session, VIDEO_LINK, mainId, COURSE_ID)
-#verbose()
-
-
-
-#printStream(streams[0], VIDEO_LINK, 0)
